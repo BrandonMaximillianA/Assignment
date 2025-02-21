f723de9fc0ddd4b533a14460558a4f82327e213b
BrandonMaximillianA <brandonavalokita@gmail.com>
2025-02-21 11:55:30 +0700

    Update commit.c

--- commit.c
+++ commit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World");
+    printf("Hello World\n"); //lupa print new line
     char laper[] = "Pizza";
     printf("Gw suka %d\n", laper);
     return 0;
